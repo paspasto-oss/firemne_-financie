e1f9c7fe5b7f0f21fb97c7f53a6f31f212616336
paspasto-oss <paspa.sto@gmail.com>
2025-10-28 20:12:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,32 @@
-const CACHE = 'fin-pwa-v1';
-const PRECACHE = [
+const CACHE = 'financie-v1';
+const ASSETS = [
   './',
   './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
+  './manifest.webmanifest',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
 
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))) .then(()=>self.clients.claim()));
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
 });
 self.addEventListener('fetch', e=>{
   const req = e.request;
-  const isHTML = req.headers.get('accept')?.includes('text/html');
-  if (isHTML) {
-    e.respondWith(
-      fetch(req).then(res=>{ caches.open(CACHE).then(c=>c.put(req,res.clone())); return res; })
-                .catch(()=>caches.match(req))
-    );
-  } else {
-    e.respondWith(
-      caches.match(req).then(hit => hit || fetch(req).then(res=>{
-        if (req.method==='GET' && res.status===200) caches.open(CACHE).then(c=>c.put(req,res.clone()));
-        return res;
-      }))
-    );
-  }
+  e.respondWith(
+    caches.match(req).then(res=> res || fetch(req).then(net=>{
+      // network-first for json imports
+      if(req.method==='GET' && req.url.startsWith(self.location.origin)){
+        const copy = net.clone();
+        caches.open(CACHE).then(c=>c.put(req, copy));
+      }
+      return net;
+    }).catch(()=>caches.match('./index.html')))
+  );
 });
